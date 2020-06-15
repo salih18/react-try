4be53ffd84ebfddec5daa3a9757f2df340b62915
salih <salihsert18@gmail.com>
2020-06-15 16:40:27 +0200

    connect strapi backend

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://yunus-strapi.herokuapp.com/'
+    ? 'https://yunus-strapi.herokuapp.com'
     : 'http://localhost:1337';
 
 export default baseUrl;
